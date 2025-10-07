3dcbe0f7c3068106b590c11a92b3afe858e54290
Carlos Petit <ca.alberto.p@gmail.com>
2025-10-07 18:26:45 -0300

    feat: update pr

--- run_proyecto_cesfam.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\darkd\Documents\ProyectosVSCUNAB\proyecto_cesfam"
-echo ==============================
-echo  Iniciando servidor Vite React
-echo ==============================
-npm run dev
-pause
